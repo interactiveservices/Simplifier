ce22e81346314c956608b434626cf27d4b8f84b7
Alex Shubin <alexshubin22@gmail.com>
2017-06-26 10:18:52 +0300

    prettyjson

--- Simplifier/Classes/CollectionSimplifier.swift
+++ Simplifier/Classes/CollectionSimplifier.swift
@@ -13,4 +13,18 @@ extension Collection where Index == Indices.Iterator.Element {
     public subscript(safe index: Index) -> Iterator.Element? {
         return indices.contains(index) ? self[index] : nil
     }
+
+    func prettyJSONString() -> String {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: self, options: [.prettyPrinted])
+            guard let jsonString = String(data: jsonData, encoding: String.Encoding.utf8) else {
+                print("Can't create string with data.:\(self)")
+                return "{}"
+            }
+            return jsonString
+        } catch let parseError {
+            print("json serialization error: \(parseError)")
+            return "{}"
+        }
+    }
 }
